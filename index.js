ac68cab6cde252c7f573974a311ebd8012a76d49
sergeichestakov <me@sergei.com>
2023-10-10 17:32:45 +0000

    Add logs and fix shadowed variable

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const http = require('http');
 const server = require('./lib/server');
 
 http.createServer((req, res) => {
+  console.log('Starting server on port 3000');
   server(req, res);
 }).listen(3000);
